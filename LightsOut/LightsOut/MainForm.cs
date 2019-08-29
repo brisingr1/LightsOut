d51eaebf19388e205156bc14cd3b028f89e1e390
Hyhway <htherrell@harding.edu>
2019-08-28 21:04:27 -0500

    Add lights out program to repo

---
+++ LightsOut/LightsOut/MainForm.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LightsOut
+{
+    public partial class MainForm : Form
+    {
+        private const int GridOffset = 25;                          // Distance from upper-left side of window
+        private const int GridLength = 200;                         // Size in pixels of grid
+        private const int NumCells = 3;                             // Number of cells in grid
+        private const int CellLength = GridLength / NumCells;
+
+        private bool[,] grid;                                       // Stores on/off state of cells in grid
+        private Random rand;                                        // Used to generate random numbers
+        public MainForm()
+        {
+            InitializeComponent();
+
+            rand = new Random();                                    // Initializes random number generator
+            grid = new bool[NumCells, NumCells];                    // Create grid, cells x cells
+
+            // Turn entire grid on
+            for (int r = 0; r < NumCells; r++)
+                for (int c = 0; c < NumCells; c++)
+                    grid[r, c] = true;
+        }
+
+        private void HelpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            // File grid with either white or black
+            for (int r = 0; r < NumCells; r++)
+                for (int c = 0; c < NumCells; c++)
+                    grid[r, c] = rand.Next(2) == 1;
+
+            // Redraw grid
+            this.Invalidate();
+        }
+
+        private void MainForm_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            for (int r = 0; r < NumCells; r++)
+            {
+                for (int c = 0; c < NumCells; c++)
+                {
+                    // Get proper pen and brush for on/off
+                    // grid section
+                    Brush brush;
+                    Pen pen;
+
+                    if (grid[r, c])
+                    {
+                        pen = Pens.Black;
+                        brush = Brushes.White;                         // On
+                    }
+                    else
+                    {
+                        pen = Pens.White;
+                        brush = Brushes.Black;                         // Off
+
+                    }
+
+                    // Determine (x,y) coord of row and col to draw rectangle
+                    int x = c * CellLength + GridOffset;
+                    int y = r * CellLength + GridOffset;
+
+                    // Draw outline and inner rectangle
+                    g.DrawRectangle(pen, x, y, CellLength, CellLength);
+                    g.FillRectangle(brush, x + 1, y + 1, CellLength - 1, CellLength - 1);
+                }
+            }
+        }
+
+        private void MainForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            // Make sure click was inside the grid
+            if (e.X < GridOffset || e.X > CellLength * NumCells + GridOffset ||
+                e.Y < GridOffset || e.Y > CellLength * NumCells + GridOffset)
+                return;
+
+            // Find row, col of mouse press
+            int r = (e.Y - GridOffset) / CellLength;
+            int c = (e.X - GridOffset) / CellLength;
+
+            // Invert selected box and all surrounding boxes
+            for (int i = r - 1; i <= r + 1; i++)
+                for (int j = c - 1; j <= c + 1; j++)
+                    if (i >= 0 && i < NumCells && j >= 0 && j < NumCells)
+                        grid[i, j] = !grid[i, j];
+
+            // Redraw grid
+            this.Invalidate();
+
+            // Check to see if puzzle has been solved
+            if (PlayerWon())
+            {
+                // Display winner dialog box
+                MessageBox.Show(this, "Congratulations! You've won!", "Lights Out!",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private bool PlayerWon()
+        {
+            bool state = true;
+
+            for (int i = 0; i < NumCells && state; i++)
+            {
+                for (int j = 0; j < NumCells && state; j++)
+                {
+                    // Check to see if grid[x,y] is true, if so then player has not won
+                    // Then set state to false 
+                    if (grid[i,j])
+                    {
+                        state = false;
+                    }
+                }
+            }
+
+            return state;
+        }
+
+        private void NewToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Button1_Click(sender, e);
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void ExitToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            ExitButton_Click(sender, e);
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AboutForm aboutBox = new AboutForm();
+            aboutBox.ShowDialog(this);
+        }
+    }
+}
